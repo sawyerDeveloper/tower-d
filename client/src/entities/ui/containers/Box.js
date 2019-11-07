30a99759afdba409788e9b72a82caaf8bd5d4047
Anthony Sawyer <Sawyer.developer@gmail.com>
2019-11-06 16:35:07 -0800

    added more box

--- client/src/entities/ui/containers/Box.js
+++ client/src/entities/ui/containers/Box.js
@@ -1,11 +1,23 @@
+import Entity from "../../Entity"
 
-class Box {
-    constructor(x, y, width, height){
+class Box extends Entity{
+    constructor(x, y, width, height, data = null){
+
+        if(data){
+            super(data)
+            this.data = data
+        }else{
+            super({})
+        }
         this.x = x
         this.y = y
         this.width = width
         this.height = height
     }
 
-    
+    render(ctx){
+        super.render(ctx)
+    }
 }
+
+export default Box
